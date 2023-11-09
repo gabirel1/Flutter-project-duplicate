ed695fbbc2a17be18119a28fde6b29262744fd44
eoxiaguillaume <70137982+eoxiaguillaume@users.noreply.github.com>
2023-11-09 18:37:09 +0100

    Adding a navigation bar to navigate between homePage loginPage and registerPage

---
+++ my_app/lib/Pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> drawerScaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.yellow,
+        body: Scaffold(
+          key: drawerScaffoldKey,
+        ),
+      ),
+    );
+  }
+}
