a44a24cdc513cadb549ca54756f792aa523916eb
gabriel knies <gabriel.knies@epitech.eu>
2023-11-20 22:23:24 +0100

    [tests]: adding test for navbar

--- my_app/test/widget_test.dart
+++ my_app/test/widget_test.dart
@@ -7,24 +7,23 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_app/main.dart';
+import 'package:my_app/Elements/bottom_navigation_bar.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('MyBottomNavigationBar test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: MyBottomNavigationBar(),
+      ),
+    );
+    expect(find.byType(MyBottomNavigationBar), findsOneWidget);
+    // list all widgets inside
+    expect(find.byType(Icon), findsNWidgets(3));
+    // check if first is 'Home'
+    expect(find.text('Home'), findsOneWidget);
+    // check if second is 'Basket'
+    expect(find.text('Basket'), findsOneWidget);
+    // check if third is 'Profile'
+    expect(find.text('Profile'), findsOneWidget);
   });
 }
