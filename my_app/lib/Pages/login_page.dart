02d9ac539d494ff9383855ab7ade45cc321b74e6
charlesdubret <charles.dubret@epitech.eu>
2023-12-02 18:41:58 +0100

    BottomNavigationBar re factorisation Single NavigationBar throughout the app Animation on page navigation

--- my_app/lib/Pages/login_page.dart
+++ my_app/lib/Pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/Elements/bottom_navigation_bar.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,18 +12,10 @@ class LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
-      child: const Scaffold(
-        backgroundColor: Colors.red,
-        body: Scaffold(
-          body: Text(
-            'Hello',
-          ),
-        ),
-        bottomNavigationBar: MyBottomNavigationBar(),
+    return const Scaffold(
+      backgroundColor: Colors.red,
+      body: Text(
+        'Hello',
       ),
     );
   }
--- my_app/lib/Pages/profile_page.dart
+++ my_app/lib/Pages/profile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/Elements/bottom_navigation_bar.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,20 +12,12 @@ class ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return false;
-      },
-      child: const Scaffold(
-        body: Scaffold(
-          backgroundColor: Colors.yellow,
-          body: Center(
-            child: Text(
-              'Hello',
-            ),
-          ),
+    return const Scaffold(
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: Text(
+          'Hello',
         ),
-        bottomNavigationBar: MyBottomNavigationBar(),
       ),
     );
   }
