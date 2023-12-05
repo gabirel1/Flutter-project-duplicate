f92562ce16cfd12417fb7be7f5c174a27bee1573
gabriel knies <gabriel.knies@epitech.eu>
2023-12-05 19:03:23 +0100

    fixing bad link to page

--- my_app/lib/Pages/authentication_page.dart
+++ my_app/lib/Pages/authentication_page.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:my_app/Elements/bottom_navigation_bar.dart';
-import 'package:my_app/Pages/profile_page.dart';
+import 'package:my_app/Pages/MainPages/profile_page.dart';
 import 'package:my_app/Repository/firestore_service.dart';
 import 'package:my_app/Store/State/app_state.dart';
 import 'package:my_app/Store/ViewModels/authentication_view_model.dart';
